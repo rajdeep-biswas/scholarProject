1ad22d89dd9ad5578574995f431d7f77145cd5db
Biswas <rajdeep.biswas@sap.com>
2020-08-28 22:21:42 +0530

    FIXED

--- src/main/java/com/sap/scholarProject/model/Scholar.java
+++ src/main/java/com/sap/scholarProject/model/Scholar.java
@@ -1,9 +1,21 @@
 package com.sap.scholarProject.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Scholar_Details")
 public class Scholar {
 	
+	@Id
 	private String iNumber;
+	
+	@Column
 	private String name;
+	
+	@Column
 	private String batch;
 
 	public String getiNumber() {
--- src/main/java/com/sap/scholarProject/service/ScholarService.java
+++ src/main/java/com/sap/scholarProject/service/ScholarService.java
@@ -1,42 +1,46 @@
 package com.sap.scholarProject.service;
 
-import java.sql.Connection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sap.scholarProject.config.DatabaseConfiguration;
 import com.sap.scholarProject.model.Scholar;
+import com.sap.scholarProject.repository.ScholarRepository;
 
 @Service
 public class ScholarService {
 	
-//	private DatabaseConfiguration dbCon;
-	private Connection conn;
-	
 	@Autowired
-	private ScholarDatabase scholarDatabase;
+	ScholarRepository scholarRepository;
 	
-	@Autowired
-	public ScholarService(DatabaseConfiguration dbCon) {
-//		this.dbCon = dbCon;
-		this.conn = dbCon.setConnection();
-	}
+////	private DatabaseConfiguration dbCon;
+//	private Connection conn;
+//	
+//	@Autowired
+//	private ScholarDatabase scholarDatabase;
+//	
+//	@Autowired
+//	public ScholarService(DatabaseConfiguration dbCon) {
+////		this.dbCon = dbCon;
+//		this.conn = dbCon.setConnection();
+//	}
 	
 	public List<Scholar> getAllScholars() {
-		return scholarDatabase.getAllData(conn);
+		return (List<Scholar>) scholarRepository.findAll();
 	}
 	
 	public Scholar getScholar(String id) {
-		return scholarDatabase.getData(conn, id);
+		Optional<Scholar> scholr = scholarRepository.findById(id);
+		return scholr.get();
 	}
 	
 	public void addScholar(Scholar scholar) {
-		scholarDatabase.enterData(conn, scholar.getiNumber(), scholar.getName(), scholar.getBatch());
+		scholarRepository.save(scholar );
 	}
 	
 	public void deleteScholar(String id) {
-		scholarDatabase.removeData(conn, id);
+//		scholarDatabase.removeData(conn, id);
 	}
 }
